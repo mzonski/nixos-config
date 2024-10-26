17ba5f009693e3cf6216ab3333ea0bfe3c4967b5
Maciej Zonski <me@zonni.pl>
2024-10-26 07:59:58 +0200

    suspend don't corrupt memory anymore

--- hosts/corn/display.nix
+++ hosts/corn/display.nix
@@ -1,9 +1,35 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
-  boot.kernelParams = [ "nvidia_drm.fbdev=1" ];
+  # 
+  boot.initrd.kernelModules = [
+    "nvidia"
+    "i915"
+    "nvidia_modeset"
+    "nvidia_uvm"
+    "nvidia_drm"
+  ];
+  boot.kernelParams = [
+    "nvidia_drm.modeset=1"
+    "nvidia_drm.fbdev=1"
+    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+    "nvidia.NVreg_OpenRmEnableUnsupportedGpus=1"
+  ];
   services.xserver = {
+
     exportConfiguration = true;
+
+    #this disables power management basically
+    extraConfig = ''
+      Section "Extensions"
+          Option "DPMS" "Disable"
+      EndSection
+
+      Section "ServerFlags"
+          Option "NoPM" "true"
+      EndSection
+    '';
+
     xrandrHeads = [
       {
         # Configure the LG 27UL850-W (primary monitor)
@@ -26,32 +52,42 @@
           DisplaySize 470 520
           Modeline "2560x2880_60" 638 2560 2784 3064 3568 2880 2883 2893 2982 -HSync +VSync
           Option "PreferredMode" "2560x2880_60"
-          Option "RightOf" "HDMI-1"
+          Option "RightOf" "HDMI-5"
         '';
       }
     ];
 
-    videoDrivers = [ "nvidia" ];
+    videoDrivers = [
+      #"intel"
+      "nvidia"
+    ];
   };
+
+  #boot.kernelParams = [
+  #  "nvidia.NVreg_PreserveVideoMemoryAllocations=1" # cfg.powerManagement.enable without services
+  #];
 
   hardware.graphics.enable = true;
   hardware.nvidia = {
     modesetting.enable = true;
+
     powerManagement.enable = true;
-    # offload = {
-    #   enable = true;
-    #   enableOffloadCmd = true;
-    # };
     powerManagement.finegrained = false;
     open = false;
-    nvidiaSettings = true;
+
+    forceFullCompositionPipeline = true;
+
     prime = {
-      reverseSync.enable = true;
-      allowExternalGpu = true;
-      #sync.enable = true;
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      #reverseSync.enable = true;
+      #allowExternalGpu = true;
+      # sync.enable = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 }
