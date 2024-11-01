a19241156f5dc43246b7f4a3d6846215e192b4d8
Maciej Zonski <me@zonni.pl>
2024-11-01 22:42:22 +0100

    Catpuccin theme for GitKraken

--- home/zonni/features/development/gitkraken.nix
+++ home/zonni/features/development/gitkraken.nix
@@ -3,13 +3,14 @@
   home.packages = (
     with pkgs;
     [
-      vscode
-      cowsay
-      nixfmt-rfc-style
-      nil
-      python312
-      python312Packages.pip
       gitkraken
     ]
   );
+
+  home.file = {
+    ".gitkraken/themes/catppuccin-mocha.jsonc".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/gitkraken/1ed3b2807f0ecbeb0a276fc17af26daf48874caf/themes/catppuccin-mocha.jsonc";
+      sha256 = "sha256-u97bjJi3V2AI8Hw9wI25KfSe4bneX0QcOU0rzmeGaMM=";
+    };
+  };
 }
