ac29b79aac4c1b63f888d8cd9b8bf9f62e654724
Maciej Zonski <me@zonni.pl>
2024-12-15 15:57:18 +0100

    add link to jetbrains issue

--- modules/home/programs/development/jetbrains.nix
+++ modules/home/programs/development/jetbrains.nix
@@ -23,6 +23,7 @@ in
       rust-rover
       datagrip
     ];
+    # https://youtrack.jetbrains.com/issue/IJPL-2176/File-watcher-failed-to-start-table-error-collision-messages-in-the-log#focus=Comments-27-11108420.0-0
     # Fixes: Watcher terminated with exit code 3 // table error: collision
     home.file."idea.properties".text =
       "idea.filewatcher.executable.path = ${pkgs.fsnotifier}/bin/fsnotifier";
