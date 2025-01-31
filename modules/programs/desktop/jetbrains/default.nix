beebce2f90a92b76d11c612ee736fb6a4e6e380e
Maciej Zonski <me@zonni.pl>
2025-01-31 20:50:21 +0100

    (jetbrains): add clion

--- modules/programs/desktop/jetbrains/default.nix
+++ modules/programs/desktop/jetbrains/default.nix
@@ -18,6 +18,7 @@ module {
       pycharm-professional
       rust-rover
       datagrip
+      clion
     ];
     # https://youtrack.jetbrains.com/issue/IJPL-2176/File-watcher-failed-to-start-table-error-collision-messages-in-the-log#focus=Comments-27-11108420.0-0
     # Fixes: Watcher terminated with exit code 3 // table error: collision
