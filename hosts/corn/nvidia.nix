ff98b6d659d90b73056d2c85067b299c1fe239e0
Maciej Zonski <me@zonni.pl>
2025-03-08 09:37:03 +0100

    (hardware): update nvidia driver

--- hosts/corn/nvidia.nix
+++ hosts/corn/nvidia.nix
@@ -36,6 +36,8 @@ delib.host {
         libvdpau-va-gl
         libva1
         libGL
+
+        egl-wayland
       ];
     };
 
@@ -53,11 +55,22 @@ delib.host {
       prime = {
         offload.enable = false;
         sync.enable = false;
+        reverseSync.enable = false;
+        allowExternalGpu = false;
 
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
-      package = config.boot.kernelPackages.nvidiaPackages.latest;
+
+      #package = config.boot.kernelPackages.nvidiaPackages.latest;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "570.124.04";
+        sha256_64bit = "sha256-G3hqS3Ei18QhbFiuQAdoik93jBlsFI2RkWOBXuENU8Q=";
+        sha256_aarch64 = "sha256-HN58N00SNEwMQKSKuOMAXVW6J2VI/2YyDulQNJHuVeM=";
+        openSha256 = "sha256-KCGUyu/XtmgcBqJ8NLw/iXlaqB9/exg51KFx0Ta5ip0=";
+        settingsSha256 = "sha256-LNL0J/sYHD8vagkV1w8tb52gMtzj/F0QmJTV1cMaso8=";
+        persistencedSha256 = "sha256-SHSdnGyAiRH6e0gYMYKvlpRSH5KYlJSA1AJXPm7MDRk=";
+      };
     };
 
     #hardware.nvidia-container-toolkit = {
