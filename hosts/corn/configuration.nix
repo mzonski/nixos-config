817c0dc4db4d03c539dc6dfd538641b3c85f397d
Maciej Zonski <me@zonni.pl>
2025-04-07 18:23:45 +0200

    (corn): tidy gnome fixes

--- hosts/corn/configuration.nix
+++ hosts/corn/configuration.nix
@@ -37,6 +37,7 @@ delib.host {
 
     programs.chrome.enable = true;
     programs.gnome.enable = true;
+    programs.gnome.noUserSessionFreeze.enable = true;
   };
 
   nixos = {
@@ -47,35 +48,6 @@ delib.host {
       inputs.nixos-hardware.nixosModules.common-cpu-intel-cpu-only
       inputs.nixos-hardware.nixosModules.common-pc-ssd
     ];
-
-    systemd.package = pkgs.unstable.systemd;
-
-    # boot.kernelParams = [
-    #   "freezer.no_user_freeze=1" # Prevents the freezer cgroup from freezing user processes
-    # ];
-
-    # # https://github.com/kachick/dotfiles/issues/959#issuecomment-2533264029
-    systemd.services.systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
-
-    systemd.services.nvidia-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
-    systemd.services.nvidia-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
-    systemd.services.pre-sleep.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
-    systemd.services.post-resume.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
-    systemd.services.systemd-hibernate-clear.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
-    systemd.services.systemd-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
-    systemd.services.systemd-hybrid-sleep.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
-    systemd.services.systemd-suspend-then-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS =
-      "false";
-
-    # systemd.sleep.extraConfig = ''
-    #   SuspendState=freeze
-    # '';
-
-    # # https://gitlab.archlinux.org/archlinux/packaging/packages/nvidia-utils/-/commit/b9ddd997381f9552131862320dcc8c4b45a60708#note_192390
-    systemd.services.homed-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
-    systemd.services.homed-hybrid-sleep.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
-    systemd.services.homed-suspend-then-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS =
-      "false";
 
     # TODO: REVERT IT
     boot.readOnlyNixStore = false;
