886ea5ea26e9d3ce7115fcc912427aac76728e2c
Maciej Zonski <me@zonni.pl>
2025-05-01 20:42:40 +0200

    (hardware:audio): no crackling HDMI output under game

--- modules/toplevel/hardware/audio.nix
+++ modules/toplevel/hardware/audio.nix
@@ -45,10 +45,49 @@ module {
             matches = [ { "node.name" = "~alsa_*"; } ]; # TODO: test bluez_input.* / bluez_output.*
             actions.update-props = {
               "session.suspend-timeout-seconds" = 0;
-              "clock.quantum-limit" = 8192;
-              #"dither.method" = "wannamaker3";
-              #"dither.noise" = 2;
             };
+          }
+        ];
+
+        wireplumber.extraConfig.NVIDIA_HDMI."monitor.alsa.rules" = [
+          {
+            matches = [ { "node.name" = "alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1"; } ];
+            actions.update-props =
+              let
+                # So much config just to not use PULSE_LATENCY_MSEC=50 variable
+                sampleRate = 192000;
+                defaultLatencyMs = 50;
+                minLatencyMs = 40;
+                maxLatencyMs = 100;
+                maxLimitMs = 200;
+
+                calcFrames = ms: ms * (sampleRate / 1000);
+
+                defaultFrames = calcFrames (defaultLatencyMs);
+                minFrames = calcFrames (minLatencyMs);
+                maxFrames = calcFrames (maxLatencyMs);
+                maxLimitFrames = calcFrames (maxLimitMs);
+
+                pulseFormat = frames: "${toString frames}/${toString sampleRate}";
+              in
+              {
+                "node.description" = "Speakers";
+                "node.latency" = "${toString defaultLatencyMs}/1000";
+
+                "clock.rate" = sampleRate;
+                "clock.quantum" = defaultFrames;
+                "clock.min-quantum" = minFrames;
+                "clock.max-quantum" = maxFrames;
+                "clock.quantum-limit" = maxLimitFrames;
+
+                "pulse.min.req" = pulseFormat (minFrames);
+                "pulse.default.req" = pulseFormat (defaultFrames);
+                "pulse.min.frag" = pulseFormat (minFrames);
+                "pulse.default.frag" = pulseFormat (defaultFrames);
+                "pulse.default.tlength" = pulseFormat (maxFrames);
+                "pulse.default.maxlength" = pulseFormat (maxLimitFrames);
+                "pulse.min.quantum" = pulseFormat (minFrames);
+              };
           }
         ];
 
