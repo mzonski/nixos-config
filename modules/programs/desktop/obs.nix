bcc1d43741e8a91a573461609be63ecb38b9b8ce
Maciej Zonski <me@zonni.pl>
2025-05-07 19:04:28 +0200

    (app:obs): refactor module

--- modules/programs/desktop/obs.nix
+++ modules/programs/desktop/obs.nix
@@ -3,33 +3,32 @@
   pkgs,
   host,
   ...
-}:
-let
+}: let
   inherit (delib) module singleEnableOption;
 in
-module {
-  name = "programs.desktop.obs";
+  module {
+    name = "programs.desktop.obs";
 
-  options = singleEnableOption host.isDesktop;
+    options = singleEnableOption host.isDesktop;
 
-  home.ifEnabled = {
-    programs.obs-studio = {
-      enable = true;
-      package = pkgs.obs-studio;
-      plugins = with pkgs; [
-        obs-studio-plugins.wlrobs
-        obs-studio-plugins.obs-pipewire-audio-capture
+    home.ifEnabled = {
+      programs.obs-studio = {
+        enable = true;
+        package = pkgs.obs-studio;
+        plugins = with pkgs.obs-studio-plugins; [
+          wlrobs
+          obs-pipewire-audio-capture
 
-        obs-studio-plugins.input-overlay
-        # obs-studio-plugins.obs-vertical-canvas (to enable, broken atm)
+          input-overlay
+          # obs-studio-plugins.obs-vertical-canvas (to enable, broken atm)
 
-        #obs-studio-plugins.obs-source-switcher
-        #obs-studio-plugins.obs-backgroundremoval
-        #obs-studio-plugins.obs-composite-blur
-        #obs-studio-plugins.obs-scale-to-sound
-      ];
+          #obs-studio-plugins.obs-source-switcher
+          #obs-studio-plugins.obs-backgroundremoval
+          #obs-studio-plugins.obs-composite-blur
+          #obs-studio-plugins.obs-scale-to-sound
+        ];
+      };
+
+      xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-wlr]; # recording via pipewire
     };
-
-    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr ]; # recording via pipewire
-  };
-}
+  }
