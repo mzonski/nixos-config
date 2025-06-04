1eac09da7a1a75b597ddef2ef9b6d1305be68542
Maciej Zonski <me@zonni.pl>
2025-06-04 22:42:25 +0200

    (toplevel:nixpkgs): solve overlays warning

--- modules/toplevel/nixpkgs.nix
+++ modules/toplevel/nixpkgs.nix
@@ -1,8 +1,44 @@
-{ delib, lib, ... }:
+{
+  delib,
+  lib,
+  inputs,
+  ...
+}:
 let
   shared.nixpkgs.config = {
     allowUnfree = true;
+    permittedInsecurePackages = [
+      "archiver-3.5.1"
+    ];
   };
+
+  shared.nixpkgs.overlays = [
+    (
+      final: prev:
+      let
+        inherit (final.stdenv.hostPlatform) system;
+        inherit (final) config;
+
+        unstable = import inputs.nixpkgs-unstable {
+          inherit system config;
+        };
+      in
+      {
+        inherit unstable;
+        hyprFlake = inputs.hyprland.packages.${system};
+        hyprPluginsFlake = inputs.hyprland-plugins.packages.${system};
+        firefoxAddons = inputs.firefox-addons.packages.${system};
+        local = builtins.listToAttrs (
+          map (path: {
+            name = baseNameOf (dirOf path);
+            value = unstable.callPackage path { inherit inputs; };
+          }) (inputs.denix.lib.umport { path = ../../packages; })
+        );
+      }
+      // (import ../../overlays) final prev
+    )
+  ];
+
   # TODO: /root/.config/nixpkgs/config.nix
   files."nixpkgs/config.nix".text = ''
     {
