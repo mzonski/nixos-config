0b2e8a06b63281ce00dc7c968ad0b479ae0e8981
Maciej Zonski <me@zonni.pl>
2025-06-08 14:58:12 +0200

    (toplevel:fonts): proper font configuration

--- modules/toplevel/fonts.nix
+++ modules/toplevel/fonts.nix
@@ -1,19 +1,45 @@
-{
-  delib,
-  host,
-  ...
-}:
+{ delib, host, ... }:
 delib.module {
   name = "font.desktop.config";
 
-  options = delib.singleEnableOption host.not.isMinimal;
+  options = delib.singleEnableOption host.isDesktop;
 
-  home.ifEnabled =
+  nixos.ifEnabled =
     { myconfig, ... }:
     {
-      home.packages = [
+      fonts.fontconfig = {
+        enable = true;
+        includeUserConf = true;
+
+        allowBitmaps = true;
+        allowType1 = false;
+        useEmbeddedBitmaps = true;
+        cache32Bit = false;
+        antialias = true;
+
+        hinting = {
+          enable = false;
+          autohint = false;
+          style = "none";
+        };
+
+        subpixel = {
+          rgba = "none";
+          lcdfilter = "none";
+        };
+
+        defaultFonts = {
+          serif = [ myconfig.rice.fonts.sans.name ];
+          sansSerif = [ myconfig.rice.fonts.sans.name ];
+          monospace = [ myconfig.rice.fonts.monospace.name ];
+          emoji = [ myconfig.rice.fonts.emoji.name ];
+        };
+      };
+
+      environment.systemPackages = [
         myconfig.rice.fonts.monospace.package
-        myconfig.rice.fonts.regular.package
+        myconfig.rice.fonts.sans.package
+        myconfig.rice.fonts.emoji.package
         myconfig.rice.cursor.package
       ];
     };
