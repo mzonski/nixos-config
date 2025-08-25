286b39ec0c2ba3df4e52940c8b7f4c2f83a6c11f
Maciej Zonski <me@zonni.pl>
2025-08-25 22:18:35 +0200

    (hardware:logitech): start sonaar automatically and assign it's priority

--- modules/hardware/logitech.nix
+++ modules/hardware/logitech.nix
@@ -1,4 +1,9 @@
-{ delib, lib, ... }:
+{
+  delib,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (delib) module boolOption;
   inherit (lib) mkIf;
@@ -21,5 +26,25 @@ module {
         # Disable power wakeup events for Logitech, Inc. Logi Bolt Receiver
         ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c548", ATTR{power/wakeup}="disabled"
       '');
+
+      services.system76-scheduler.assignments."games".matchers = [
+        "\"${pkgs.solaar}/bin/.solaar-wrapped\""
+      ];
     };
+
+  home.ifEnabled = {
+    systemd.user.services.solaar = {
+      Unit = {
+        Description = "Solaar - Logitech Unifying Receiver configuration tool";
+        After = [ "graphical-session.target" ];
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.solaar}/bin/solaar --window=hide";
+        Restart = "on-failure";
+      };
+    };
+  };
 }
