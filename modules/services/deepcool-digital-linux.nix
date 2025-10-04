4cd756f4f721fdaff0cca073b34b83eea1f2ae4d
Maciej Zonski <me@zonni.pl>
2025-10-04 09:47:24 +0200

    (services:deepcool-digital-linux): add arbitrary udev rules

--- modules/services/deepcool-digital-linux.nix
+++ modules/services/deepcool-digital-linux.nix
@@ -1,10 +1,9 @@
 {
   delib,
-  pkgs,
   inputs,
+  pkgs,
   ...
 }:
-
 let
   inherit (delib) module singleEnableOption;
 in
@@ -28,5 +27,15 @@ module {
           updateMs = 2000;
         };
       };
+
+      services.udev.packages = [
+        (pkgs.writeTextDir "etc/udev/rules.d/42-deepcool-digital-linux-custom.rules" ''
+          # Intel RAPL energy usage file
+          ACTION=="add", SUBSYSTEM=="powercap", KERNEL=="intel-rapl:0", RUN+="${pkgs.coreutils}/bin/chmod 444 /sys/class/powercap/intel-rapl/intel-rapl:0/energy_uj"
+
+          # DeepCool HID raw devices
+          SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3633", MODE="0666"
+        '')
+      ];
     };
 }
