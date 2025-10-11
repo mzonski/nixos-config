3ecefc60b38cab1af2ed4f5d282b5d7491ae91ca
Maciej Zonski <me@zonni.pl>
2025-10-11 22:19:00 +0200

    (programs:text-editor): configure xdg

---
+++ modules/programs/desktop/text-editor.nix
@@ -0,0 +1,20 @@
+{ delib, host, ... }:
+let
+  inherit (delib) singleEnableOption module;
+in
+module {
+  name = "programs.desktop.textEditor";
+
+  options = singleEnableOption host.isDesktop;
+
+  myconfig.ifEnabled.xdg.mime.recommended =
+    let
+      launcher = "org.gnome.TextEditor.desktop";
+    in
+    {
+      "text/plain" = [ launcher ];
+      "text/json" = [ launcher ];
+      "application/octet-stream" = [ launcher ];
+      "application/x-desktop" = [ launcher ];
+    };
+}
