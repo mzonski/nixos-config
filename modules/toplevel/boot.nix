63c86d161ce6f6f04b026f0b074341e7f6681e00
Maciej Zonski <me@zonni.pl>
2025-11-09 18:27:43 +0100

    (toplevel:boot): don't remove fallback boot entry

--- modules/toplevel/boot.nix
+++ modules/toplevel/boot.nix
@@ -25,10 +25,10 @@ delib.module {
           edk2-uefi-shell.enable = false;
           configurationLimit = 10;
 
-          extraInstallCommands = ''
-            ${pkgs.coreutils}/bin/rm -f /boot/EFI/BOOT/BOOTX64.EFI
-            [ -d /boot/EFI/BOOT ] && ${pkgs.coreutils}/bin/rmdir /boot/EFI/BOOT || true
-          '';
+          # extraInstallCommands = ''
+          #   ${pkgs.coreutils}/bin/rm -f /boot/EFI/BOOT/BOOTX64.EFI
+          #   [ -d /boot/EFI/BOOT ] && ${pkgs.coreutils}/bin/rmdir /boot/EFI/BOOT || true
+          # '';
         };
       };
     };
