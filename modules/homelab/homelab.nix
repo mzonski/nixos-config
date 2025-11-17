59d5e659df6efaa44eca8a4b4baa6e9b515feda9
Maciej Zonski <me@zonni.pl>
2025-11-17 22:15:36 +0100

    (homelab): add default "db" group

--- modules/homelab/homelab.nix
+++ modules/homelab/homelab.nix
@@ -1,11 +1,18 @@
 { delib, ... }:
 let
-  inherit (delib) module strOption;
+  inherit (delib)
+    module
+    strOption
+    ;
 in
 module {
   name = "homelab";
 
   options.homelab = {
     domain = strOption "local.zonni.pl";
+  };
+
+  nixos.always = {
+    users.groups.db.gid = 3000;
   };
 }
