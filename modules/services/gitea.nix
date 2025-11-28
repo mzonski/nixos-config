5fbde36cec7cabc8d13beb83927497b1b573af72
Maciej Zonski <me@zonni.pl>
2025-11-29 00:50:30 +0100

    (services:gitea): improve configuration

--- modules/services/gitea.nix
+++ modules/services/gitea.nix
@@ -1,6 +1,5 @@
 {
   delib,
-  pkgs,
   config,
   host,
   ...
@@ -22,7 +21,7 @@ module {
     enable = boolOption false;
     dbDir = strOption "/nas/database/${username}";
     uiPort = intOption 8084;
-    sshClonePort = intOption 2222;
+    sshClonePort = intOption 22;
   };
 
   myconfig.ifEnabled =
@@ -86,21 +85,74 @@ module {
 
         appName = "Homelab Git";
         camoHmacKeyFile = config.sops.secrets.gitea_camo_hmac.path;
-        settings = {
-          server = {
-            HTTP_PORT = cfg.uiPort;
-            DOMAIN = "git.${host.name}.${myconfig.homelab.domain}";
-            ROOT_URL = "https://git.${host.name}.${myconfig.homelab.domain}/";
-            # STATIC_ROOT_PATH <- check via repl
-            SSH_PORT = cfg.sshClonePort;
-          };
-          other = {
-            SHOW_FOOTER_VERSION = false;
-          };
+        settings =
+          let
+            domain = "https://git.${host.name}.${myconfig.homelab.domain}/";
+          in
+          {
+            repository = {
+              DISABLE_HTTP_GIT = true;
+              ACCESS_CONTROL_ALLOW_ORIGIN = domain;
+              DEFAULT_REPO_UNITS = "repo.code,repo.actions";
+            };
+            "repository.pull-request" = {
+              DEFAULT_MERGE_STYLE = "squash";
+            };
+            "repository.upload" = {
+              ALLOWED_TYPES = "*/*";
+            };
+            "repository.release" = {
+              ALLOWED_TYPES = "*/*";
+            };
+            cors = {
+              ALLOW_DOMAIN = domain;
+            };
+            ui = {
+              SHOW_USER_EMAIL = false;
+            };
+            "ui.meta" = {
+              AUTHOR = "Homelab";
+              DESCRIPTION = "Homelab Git";
+            };
+            server = {
+              HTTP_PORT = cfg.uiPort;
+              DOMAIN = "git.${host.name}.${myconfig.homelab.domain}";
+              ROOT_URL = domain;
+              SSH_PORT = cfg.sshClonePort;
+              LANDING_PAGE = "login";
+            };
+            security = {
+              INSTALL_LOCK = true;
+            };
 
-          service.DISABLE_REGISTRATION = true;
-          # session.COOKIE_SECURE = true;
-        };
+            service = {
+              DISABLE_REGISTRATION = true;
+              ENABLE_BASIC_AUTHENTICATION = false;
+              ENABLE_PASSWORD_SIGNIN_FORM = false;
+
+            };
+            "service.explore" = {
+              REQUIRE_SIGNIN_VIEW = true;
+            };
+            cron = {
+              ENABLED = true;
+              SCHEDULE = "0 4 * * 0";
+            };
+            i18n = {
+              LANGS = "en-US,pl-PL";
+              NAMES = "English,Polski";
+            };
+            other = {
+              SHOW_FOOTER_VERSION = false;
+              SHOW_FOOTER_TEMPLATE_LOAD_TIME = false;
+              SHOW_FOOTER_POWERED_BY = false;
+              ENABLE_SITEMAP = false;
+              ENABLE_FEED = false;
+            };
+            api = {
+              ENABLE_SWAGGER = false;
+            };
+          };
       };
     };
 }
