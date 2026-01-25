599ac7cf7f4913d2784b20829dcc091d0be864e3
Maciej Zonski <me@zonni.pl>
2026-01-25 10:14:04 +0100

    (packages:apple-fonts): update

--- packages/apple-fonts/default.nix
+++ packages/apple-fonts/default.nix
@@ -7,16 +7,16 @@
 }:
 stdenv.mkDerivation rec {
   pname = "apple-fonts";
-  version = "1.3";
+  version = "1.4";
 
   pro = fetchurl {
     url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
-    sha256 = "sha256-Lk14U5iLc03BrzO5IdjUwORADqwxKSSg6rS3OlH9aa4=";
+    sha256 = "sha256-u7cLbIRELSNFUa2OW/ZAgIu6vbmK/8kXXqU97xphA+0=";
   };
 
   compact = fetchurl {
     url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
-    sha256 = "sha256-CMNP+sL5nshwK0lGBERp+S3YinscCGTi1LVZVl+PuOM=";
+    sha256 = "sha256-mcMM/cbmOA5ykyIb74bid9vU6wyl8nVwkvkd+VlOdwo=";
   };
 
   mono = fetchurl {
