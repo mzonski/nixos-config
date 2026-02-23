79f90c244ebf2cb3f50cabaceb1c7c049636a234
Maciej Zonski <me@zonni.pl>
2026-02-24 00:45:29 +0100

    (feature:general-development): add dotnet

--- modules/features/general-development.nix
+++ modules/features/general-development.nix
@@ -41,16 +41,60 @@ module {
         pkgs.python312Packages.requests
         pkgs.python312Packages.xmltodict
       ];
+
+      script = ''
+        echo test
+        ls "$out"
+        for i in $out/share/dotnet/sdk/*
+        do
+          i=$(basename $i)
+          length=$(printf "%s" "$i" | wc -c)
+          substring=$(printf "%s" "$i" | cut -c 1-$(expr $length - 2))
+          i="$substring""00"
+
+          echo $i
+
+          mkdir -p $out/share/dotnet/metadata/workloads/''${i/-*}
+          touch $out/share/dotnet/metadata/workloads/''${i/-*}/userlocal
+        done
+      '';
+
+      sdkOverride = (
+        finalAttrs: previousAttrs: {
+          src = (
+            previousAttrs.src.overrideAttrs (
+              finalAttrs: previousAttrs: {
+
+                postBuild = (previousAttrs.postBuild or "") + script;
+              }
+            )
+          );
+        }
+      );
+      dotnet-full =
+        with pkgs;
+        (
+          with dotnetCorePackages;
+          combinePackages [
+            (sdk_9_0.overrideAttrs sdkOverride)
+            (sdk_8_0.overrideAttrs sdkOverride)
+          ]
+        );
     in
     {
-      home.packages = node22 ++ python312 ++ kubernetes;
+      home.packages = node22 ++ python312 ++ kubernetes ++ [ dotnet-full ];
 
       xdg.configFile."npm/npmrc".text = ''
         update-notifier=false
       '';
 
+      home.sessionPath = [ "${homeconfig.home.homeDirectory}/.dotnet/tools/" ];
+
       home.sessionVariables = {
         NPM_CONFIG_USERCONFIG = "${homeconfig.home.homeDirectory}/.config/npm/npmrc";
+        DOTNET_ROOT = "${dotnet-full}/share/dotnet";
+        DOTNET_PATH = "${dotnet-full}/bin/dotnet";
+        DOTNET_CLI_TELEMETRY_OPTOUT = "true";
       };
     };
 }
