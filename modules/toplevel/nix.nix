90c028e9aec7b4b78b530dbe79fe8f6ca6c0d9b4
Maciej Zonski <me@zonni.pl>
2026-03-09 16:58:24 +0100

    (flake): organize global substituters

--- modules/toplevel/nix.nix
+++ modules/toplevel/nix.nix
@@ -13,6 +13,14 @@ let
         "flakes"
       ];
       warn-dirty = false;
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
   mkConfig = extras: lib.recursiveUpdate shared extras;
@@ -20,13 +28,12 @@ in
 delib.module {
   name = "nix";
 
-  nixos.always =
-    {
-      imports = [ inputs.flake-programs-sqlite.nixosModules.programs-sqlite ];
-    }
-    // mkConfig {
-      nix.package = lib.mkForce pkgs.nixVersions.stable;
-    };
+  nixos.always = {
+    imports = [ inputs.flake-programs-sqlite.nixosModules.programs-sqlite ];
+  }
+  // mkConfig {
+    nix.package = lib.mkForce pkgs.nixVersions.stable;
+  };
 
   home.always = mkConfig {
     nix.package = lib.mkDefault pkgs.nixVersions.stable;
