79db095e7e672896d1fc5600ca100ea4f5956008
Maciej Zonski <me@zonni.pl>
2026-05-13 21:17:07 +0200

    (corn): add reset-on-resume quirk patch for AMD 800 Series Chipset USB 3.x XHCI Controller  84ca03

--- hosts/corn/hardware-configuration.nix
+++ hosts/corn/hardware-configuration.nix
@@ -50,6 +50,13 @@ delib.host {
           kernel = config.boot.kernelPackages.kernel;
         })
       ];
+
+      kernelPatches = [
+        {
+          name = "amd-800-usb3.x-xhci-reset-on-resume";
+          patch = ./amd-800-usb-3.x-xhci-reset-on-resume.patch;
+        }
+      ];
     };
 
     # boot.kernelParams = [
